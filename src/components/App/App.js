d040a5aaa159b81328e9661771c3b3fa16f7fa08
Mikey Pinaud <michael.pinaud@modop.com>
2018-12-09 21:19:41 -0800

    create campsites json and set App initial state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,6 +15,8 @@ import Menu from '../Menu/Menu';
 import Navigation from '../Navigation/Navigation';
 import Routes from './Routes';
 
+import campList from '../../Data/campsites.json';
+
 // Create theme
 const muiTheme = createMuiTheme(campMuiTheme);
 
@@ -26,27 +28,14 @@ const styles = () => ({
 
 class App extends Component {
     state = {
-        campgrounds: [
-            {
-                name: 'Trillium Lake Campground',
-                id: '42k4lkj245lkj423klj',
-                photo:
-                    'http://raredelights.com/wp-content/uploads/2014/07/Trillium-Lake-in-Oregon-View-640x375.jpg',
-            },
-            {
-                name: 'Battle Ground State Park',
-                id: '42k4lkdlkfjfgllkgb29',
-                photo:
-                    'https://upload.wikimedia.org/wikipedia/commons/f/f7/Battle_Ground_Lake_State_Park_%28September%2C_2009%29.jpg',
-            },
-            {
-                name: 'Lazy Bend',
-                id: '24546hth245lklkjklj',
-                photo:
-                    'https://www.outdoorproject.com/sites/default/files/styles/odp_header_adaptive/public/features/dsc_0660.jpg?itok=bVuveL9h',
-            },
-        ],
+        campgrounds: [],
     };
+
+    componentDidMount() {
+        this.setState({
+            campgrounds: campList
+        })
+    }
 
     render() {
         const {classes} = this.props;
@@ -63,7 +52,7 @@ class App extends Component {
                         {/* Hide Menu in Mobile View */}
                         <Hidden xsDown>
                             {/* Elevation represents shadow intensity */}
-                            <Paper elevation={2} className={classes.paper}>
+                            <Paper elevation={1} className={classes.paper}>
                                 {campgrounds.map(campground => (
                                     <Menu
                                         key={campground.id}
