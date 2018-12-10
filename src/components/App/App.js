ce9015dcffdee6f7185b1f247f2843faa5d67e77
Mikey Pinaud <michael.pinaud@modop.com>
2018-12-09 17:45:46 -0800

    create muiTheme and CssBaseLine

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
+
+// Material
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import campMuiTheme from './campMuiTheme';
+
+// Components
 import Routes from './Routes';
+
+
+const muiTheme = createMuiTheme(campMuiTheme);
 
 class App extends Component {
     render() {
         return(
             <>
-                <div className="App">campground</div>
-                <Routes/>
+                <CssBaseline />
+                <MuiThemeProvider theme={muiTheme}>
+                    <div className="App">campground</div>
+                    <Routes/>
+                </MuiThemeProvider>
             </>
         );
     }
